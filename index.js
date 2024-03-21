00197eacc5cd18120a5b5c34fc06cb70721751b2
lema samuel <lemasam10@gmail.com>
2024-03-21 13:11:13 +0300

    aithmetic lab  done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+
+let num1 =  2
+let num2 = 31
+let multiply = num1 * num2
+
+
+
+let random = 1
+
+
+let num3 = 8
+let num4= 2
+let mod= num3 / num4 
+
+let max = Math.max(1,2,4,6,20) 
